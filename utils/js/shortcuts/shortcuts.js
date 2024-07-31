837634e6b79c01e7c3859b2cfc9b89873284e417
VanLeeFXP <litran39@hotmail.com>
2024-07-31 13:40:27 +0800

    修正 `addExternalCSS`

--- utils/js/shortcuts/shortcuts.js
+++ utils/js/shortcuts/shortcuts.js
@@ -31,7 +31,7 @@ const $frag = doc.createDocumentFragment.bind ( doc );
 
 const addExternalCSS = ( ...urls ) => {
     urls.forEach ( ( url ) => {
-        if ( doc.head.querySelector ( `link[rel=stylesheet][href="${url}"]` ) != null ) {
+        if ( doc.head.querySelector ( `link[rel=stylesheet][href="${url}"]` ) == null ) {
             // Add only if the specified stylesheet is not present
             doc.head.append ( $ele ( "link", { href: url, rel: "stylesheet" } ) );
             console.log ( "CSS Added: ", url );
