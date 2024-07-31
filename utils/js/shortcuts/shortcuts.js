18870f014e98df005654a9d08e4275e5ff89c6e1
VanLeeFXP <litran39@hotmail.com>
2024-07-31 13:19:07 +0800

    新增部分通用的 CSS 和 JS 文件

---
+++ utils/js/shortcuts/shortcuts.js
@@ -0,0 +1,30 @@
+const doc = document;
+
+const $ele = ( name = "div", options = null ) => {
+    const ele = doc.createElement ( name, options );
+    if ( options != null ) {
+        delete options.is;
+        if ( options.hasOwnProperty ( "cls" ) ) {
+            const cls = options.cls;
+            if ( Array.isArray ( cls ) ) {
+                // classes passed as array
+                ele.classList.add ( ...cls );
+            }
+            else {
+                // classes passed as string
+                ele.classList.add ( ...cls.split ( /\s+/g ) );
+            }
+            delete options.cls;
+        }
+        if ( options.hasOwnProperty ( "data" ) ) {
+            // data set
+            Object.assign ( ele.dataset, options.data );
+            delete options.data;
+        }
+        Object.assign ( ele, options );
+    }
+    return ele;
+}
+
+const $text = doc.createTextNode.bind ( doc );
+const $frag = doc.createDocumentFragment.bind ( doc );
