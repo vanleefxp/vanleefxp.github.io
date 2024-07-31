f8ede746acb2dee713fa003497a1ce4b3b63088c
VanLeeFXP <litran39@hotmail.com>
2024-07-31 14:02:07 +0800

    增加为萌娘百科 TamperMonkey 插件专用的样式表

--- utils/js/shortcuts/shortcuts.js
+++ utils/js/shortcuts/shortcuts.js
@@ -34,7 +34,6 @@ const addExternalCSS = ( ...urls ) => {
         if ( doc.head.querySelector ( `link[rel=stylesheet][href="${url}"]` ) == null ) {
             // Add only if the specified stylesheet is not present
             doc.head.append ( $ele ( "link", { href: url, rel: "stylesheet" } ) );
-            console.log ( "CSS Added: ", url );
         }
     } );
 }
